ecd83c43d3b54f7f573e807a2cba2c8a6ae92f70
Harif <harifsk09@gmail.com>
2022-09-03 16:09:53 +0530

    corrected

--- outputs.tf
+++ outputs.tf
@@ -3,39 +3,39 @@ output "this_vpc_id" {
     value = aws_vpc.this_vpc.id
 }
 
-# output "this_vpc_arn" {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.arn
-# }
+output "this_vpc_arn" {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.arn
+}
 
-# output "this_vpc_instance_tenancy" {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.instance_tenancy
-# }
+output "this_vpc_instance_tenancy" {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.instance_tenancy
+}
 
-# output "this_vpc_enable_dns_support " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.enable_dns_support 
-# }
+output "this_vpc_enable_dns_support " {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.enable_dns_support 
+}
 
-# output "this_vpc_enable_dns_hostnames " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.enable_dns_hostnames 
-# }
+output "this_vpc_enable_dns_hostnames " {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.enable_dns_hostnames 
+}
 
-# output "this_vpc_enable_dns_hostnames " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.enable_dns_hostnames 
-# }
+output "this_vpc_enable_dns_hostnames " {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.enable_dns_hostnames 
+}
 
-# output "this_vpc_enable_classiclink " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.enable_classiclink
-# }
+output "this_vpc_enable_classiclink " {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.enable_classiclink
+}
 
-# output "this_vpc_main_route_table_id " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.main_route_table_id
-# }
+output "this_vpc_main_route_table_id " {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.main_route_table_id
+}
 
 
