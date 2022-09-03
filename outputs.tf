43b65898162d429002b2fd53fe1fbca8db1f0f5a
Harif <harifsk09@gmail.com>
2022-09-03 16:14:42 +0530

    corrected

--- outputs.tf
+++ outputs.tf
@@ -13,29 +13,29 @@ output "this_vpc_instance_tenancy" {
     value = aws_vpc.this_vpc.instance_tenancy
 }
 
-output "this_vpc_enable_dns_support " {
+output "this_vpc_enable_dns_support" {
     description = "The ID of the VPC"
     value = aws_vpc.this_vpc.enable_dns_support 
 }
 
-output "this_vpc_enable_dns_hostnames " {
+output "this_vpc_enable_dns_hostnames" {
     description = "The ID of the VPC"
     value = aws_vpc.this_vpc.enable_dns_hostnames 
 }
 
-# output "this_vpc_enable_dns_hostnames " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.enable_dns_hostnames 
-# }
+output "this_vpc_enable_dns_hostnames" {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.enable_dns_hostnames 
+}
 
-# output "this_vpc_enable_classiclink " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.enable_classiclink
-# }
+output "this_vpc_enable_classiclink" {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.enable_classiclink
+}
 
-# output "this_vpc_main_route_table_id " {
-#     description = "The ID of the VPC"
-#     value = aws_vpc.this_vpc.main_route_table_id
-# }
+output "this_vpc_main_route_table_id" {
+    description = "The ID of the VPC"
+    value = aws_vpc.this_vpc.main_route_table_id
+}
 
 
