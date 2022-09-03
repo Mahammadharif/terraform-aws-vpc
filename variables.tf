0fe789edf9a1ad4d22b06d900cbd6abaef8c7491
Harif <harifsk09@gmail.com>
2022-09-03 16:27:32 +0530

    added possible arguments

--- variables.tf
+++ variables.tf
@@ -9,3 +9,19 @@ variable "vpc_name" {
   type        = string
   default     = ""
 }
+
+variable "vpc_dns_support" {
+  description = "[Optional] Specify vpc dns support "
+  type        = boolean
+  default     = true # this is the default value from terraform 
+}
+
+variable "enable_dns_hostnames" {
+  description = "[Optional] Specify DNS hostname resolution "
+  type        = boolean
+  default     = false # this is the default value from terraform 
+}
+
+
+
+
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "this_vpc" {
   cidr_block = var.vpc_cidr
+  enable_dns_support = var.vpc_dns_support
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
   tags = {
     Name = local.vpc_name
   }
